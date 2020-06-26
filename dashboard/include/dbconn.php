60da58d95d03f963721deb3b9a92a5b859a1c0fb
Jos33y <josephlagbalu@gmail.com>
2020-06-26 23:18:53 +0100

    adding the php codes

---
+++ dashboard/include/dbconn.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+$con = mysqli_connect("localhost", "root", "", "showroom");
+
+
+//$con = mysqli_connect("localhost", "u721127448_Admin", "LagbaluJ19", "u721127448_eacez");
+
+function getRealIpUser(){
+
+            switch(true){
+        
+                    case(!empty($_SERVER['HTTP_X_REAL_IP'])) : return $_SERVER['HTTP_X_REAL_IP'];
+                    case(!empty($_SERVER['HTTP_CLIENT_IP'])) : return $_SERVER['HTTP_CLIENT_IP'];
+                    case(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) : return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        
+                    default : return $_SERVER['REMOTE_ADDR'];
+            }
+        }
+?>
--- dashboard/insert-product.php
+++ dashboard/insert-product.php
@@ -1,7 +1,11 @@
 <?php 
 
 $pageTitle =  "insert products";
+include('include/dbconn.php');
 include('header.php');
+
+
+
 
  ?>
     <!--TODO:  Translate the code to PHP Codes-->
@@ -13,22 +17,30 @@ include('header.php');
             <div class="col-md-4"></div>
             <div class="col-md-4">
                 <h3 class="dash-title text-center"> <i class="fas fa-file-upload"></i> Insert Product</h3>
-                <form method="post" class="form">
+
+                <?php if(!empty($msg)){ ?>
+                 <div class="alert <?php echo $css_class; ?>">
+                 <?php echo $msg; ?>
+                 </div>
+
+                <?php } ?>
+
+                <form method="post" class="form" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="Product Name" class="sr-only">Product Name</label>
-                        <input type="text" name="prod-name" class="form-control-md form-control-lg" id=""
+                        <input type="text" name="prodName" class="form-control-md form-control-lg" id=""
                             placeholder="Product Name">
                     </div>
 
                     <div class="form-group">
                         <label for="Product Price" class="sr-only">Product Price</label>
-                        <input type="text" name="prod-price" class="form-control-md form-control-lg" id=""
+                        <input type="text" name="prodPrice" class="form-control-md form-control-lg" id=""
                             placeholder="Product Price">
                     </div>
 
                     <div class="form-group">
                         <label for="new-password-2" class="sr-only">Product Image</label>
-                        <input type="file" name="prod-img" class="form-control-md form-control-lg" id=""
+                        <input type="file" name="prodImage" class="form-control-md form-control-lg" id=""
                             placeholder="Product Image">
                     </div>
 
@@ -53,3 +65,27 @@ include('header.php');
 </body>
 
 </html>
+
+<?php 
+if(isset($_POST['insert'])){
+
+    $msg = "";
+    $css_class = "";
+
+    $prod_name = $_POST['prodName'];
+    $prod_price = $_POST['prodPrice'];
+
+    $prod_image = time() . '_' . $_FILES['prodImage']['name'];
+
+    $target = 'img/' . $prod_image;
+
+    if(move_uploaded_file($_FILES['prodImage']['tmp_name'], $target)){
+ 
+        echo "<script>alert('inserted successfully')</script>";
+    }
+    else{
+        $msg = "Failed to upload product";
+        $css_class = "alert-danger";
+    }
+}
+?>
