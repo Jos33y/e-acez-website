073c3070db761659d5276a9c4f7d75b163fcc912
Jos33y <josephlagbalu@gmail.com>
2020-06-27 01:53:36 +0100

    updated database for showroom

--- showroom.php
+++ showroom.php
@@ -40,9 +40,9 @@
 
             <?php 
 
-            $con = mysqli_connect("localhost", "root", "", "showroom");
+           // $con = mysqli_connect("localhost", "root", "", "showroom");
 
-        //$con = mysqli_connect("localhost", "u721127448_joseey", "LagbaluJ19", "u721127448_showroom");
+        $con = mysqli_connect("localhost", "u721127448_joseey", "LagbaluJ19", "u721127448_showroom");
 
             $i = 0;
 
